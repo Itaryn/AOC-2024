b8517ae450f555f945a4529f3cf08568155037d1
itaryn <g.prevot7@gmail.com>
2024-12-29 11:36:31 +0100

    Day 17

--- src/main/java/aoc/day17/Day17.java
+++ src/main/java/aoc/day17/Day17.java
@@ -6,10 +6,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -107,26 +104,180 @@ public class Day17 implements DailyExercise {
     protected Long getRegisterABruteForce(List<Long> register, List<Integer> program) {
         List<Long> out;
         List<Long> registerCopy;
-        long registerATest = (long) Math.pow(8, program.size()) - 1;
+        long registerATest = (long) Math.pow(8, program.size() - 1);
         do {
-            registerATest -= 1;
+            registerATest++;
             registerCopy = new ArrayList<>(register);
             registerCopy.set(0, registerATest);
             out = new Day17().run(registerCopy, program);
             if (out.size() != program.size()) {
                 throw new RuntimeException("This should not happen");
             }
-            if (out.subList(0, 8).stream().map(i -> Long.toString(i)).collect(joining(","))
-                    .equals(program.subList(0, 8).stream().map(i -> Integer.toString(i)).collect(joining(",")))) {
-                System.out.printf("%s gives %s\n", Long.toBinaryString(registerATest), out.stream().map(i -> Long.toString(i)).collect(joining(",")));
-            }
-            if (out.subList(8, 16).stream().map(i -> Long.toString(i)).collect(joining(","))
-                    .equals(program.subList(8, 16).stream().map(i -> Integer.toString(i)).collect(joining(",")))) {
-                System.out.printf("%s gives %s\n", Long.toBinaryString(registerATest), out.stream().map(i -> Long.toString(i)).collect(joining(",")));
-            }
         } while (!out.stream().map(i -> Long.toString(i)).collect(joining(","))
                 .equals(program.stream().map(i -> Integer.toString(i)).collect(joining(","))));
         return registerATest;
+    }
+
+    protected List<Integer> getPossibleValues(List<Integer> program, int numberToFound) {
+        if (!List.of(5, 5, 0, 3, 3, 0).equals(program.subList(program.size() - 6, program.size()))) {
+            throw new RuntimeException("This function can't found possible values for your program");
+        }
+        List<Integer> possibleValues = new ArrayList<>();
+        // The limit is 1024 exclude because the number can't be shifted more than 7 times
+        for (int i = 0; i < 1024; i++) {
+            List<Long> register = new ArrayList<>(List.of((long) i, 0L, 0L));
+            int pointer = 0;
+            long out = -1;
+            while (out == -1) {
+                switch (program.get(pointer)) {
+                    case 0 -> adv(register, program.get(pointer + 1));
+                    case 1 -> bxl(register, program.get(pointer + 1));
+                    case 2 -> bst(register, program.get(pointer + 1));
+                    case 3 -> pointer = jnz(register, program.get(pointer + 1), pointer);
+                    case 4 -> bxc(register);
+                    case 5 -> out = out(register, program.get(pointer + 1));
+                    case 6 -> bdv(register, program.get(pointer + 1));
+                    case 7 -> cdv(register, program.get(pointer + 1));
+                }
+                pointer += 2;
+            }
+            if (out == numberToFound) {
+                possibleValues.add(i);
+            }
+        }
+
+        return possibleValues;
+    }
+
+    protected Map<Integer, List<Integer>> getPossibleValues(List<Integer> program) {
+        Map<Integer, List<Integer>> possibleProgramValues = new HashMap<>();
+        for (int i = program.size() - 1; i >= 0; i--) {
+            List<Integer> possibleValues = getPossibleValues(program, program.get(i));
+            // Last element should be less than 8
+            if (i == program.size() - 1) {
+                possibleValues = possibleValues.stream().filter(v -> v < 8).toList();
+            } else if (i == program.size() - 2) {
+                possibleValues = possibleValues.stream().filter(v -> v < Math.pow(8, 2)).toList();
+            } else if (i == program.size() - 3) {
+                possibleValues = possibleValues.stream().filter(v -> v < Math.pow(8, 3)).toList();
+            }
+            possibleProgramValues.put(i, possibleValues);
+        }
+        return possibleProgramValues;
+    }
+
+    protected Integer[] getForcedValues(List<Integer> program, Map<Integer, List<Integer>> possibleValues) {
+        int byteSize = program.size() * 3;
+        Integer[] forcedValues = new Integer[byteSize];
+        Arrays.fill(forcedValues, -1);
+        Map<Integer, List<Integer>> possibleValuesFiltered = new HashMap<>();
+        for (int i = program.size() - 1; i >= 0; i--) {
+            // Last element should be less than 8
+            if (i == program.size() - 1) {
+                possibleValuesFiltered.put(i, possibleValues.get(i).stream().filter(v -> v < 8).toList());
+                setForcedValues(forcedValues, possibleValuesFiltered.get(i), 0, 3);
+            } else if (i == program.size() - 2) {
+                possibleValuesFiltered.put(i, possibleValues.get(i).stream().filter(v -> v < Math.pow(8, 2) &&
+                        isValidWithForcedValues(List.of(forcedValues).subList(0, 6), v)).toList());
+                setForcedValues(forcedValues, possibleValuesFiltered.get(i), 0, 6);
+            } else if (i == program.size() - 3) {
+                possibleValuesFiltered.put(i, possibleValues.get(i).stream().filter(v -> v < Math.pow(8, 3) &&
+                        isValidWithForcedValues(List.of(forcedValues).subList(0, 9), v)).toList());
+                setForcedValues(forcedValues, possibleValuesFiltered.get(i), 0, 9);
+            } else {
+                int finalI = i;
+                possibleValuesFiltered.put(i, possibleValues.get(i).stream()
+                        .filter(v -> isValidWithForcedValues(List.of(forcedValues).subList(byteSize - finalI * 3 - 10, byteSize - finalI * 3), v)).toList());
+                setForcedValues(forcedValues, possibleValuesFiltered.get(i), byteSize - i * 3 - 10, byteSize - i * 3);
+            }
+        }
+        return forcedValues;
+    }
+
+    protected boolean isValidWithForcedValues(List<Integer> forcedValues, Integer v) {
+        for (int i = 0; i < forcedValues.size(); i++) {
+            Integer bit = forcedValues.get(forcedValues.size() - 1 - i);
+            if (bit != -1 && ((v & (1 << i)) > 0 && bit == 0 || (v & (1 << i)) == 0 && bit == 1)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private void setForcedValues(Integer[] forcedValues, List<Integer> possibleValues, int start, int end) {
+        while (start < forcedValues.length && forcedValues[start] == 0) {
+            start++;
+        }
+        for (int i = start; i < end; i++) {
+            int finalI = i - start;
+            if (possibleValues.stream().allMatch(v -> (v & (1 << finalI)) > 0)) {
+                if (forcedValues[end - 1 - finalI] == 0) {
+                    throw new RuntimeException("This should never happen, because possibleValues have been filtered");
+                }
+                forcedValues[end - 1 - finalI] = 1;
+            } else if (possibleValues.stream().allMatch(v -> (v & (1 << finalI)) == 0)) {
+                if (forcedValues[end - 1 - finalI] == 1) {
+                    throw new RuntimeException("This should never happen, because possibleValues have been filtered");
+                }
+                forcedValues[end - 1 - finalI] = 0;
+            }
+        }
+    }
+
+    protected long getMinRegisterA(List<Integer> program) {
+        Map<Integer, List<Integer>> possibleValues = getPossibleValues(program);
+        possibleValues = getPossibleValuesFiltered(possibleValues, getForcedValues(program, possibleValues));
+        List<Long> generatedValidValues = getValidValues(program, possibleValues);
+        return generatedValidValues.stream().min(Long::compare).orElseThrow(() -> new RuntimeException("Something went wrong"));
+    }
+
+    protected List<Long> getValidValues(List<Integer> program, Map<Integer, List<Integer>> possibleValuesMap) {
+        List<String> validValues = new ArrayList<>();
+        for (int i = program.size() - 1; i >= 0; i--) {
+            List<Integer> possibleValues = possibleValuesMap.get(i);
+            if (validValues.isEmpty()) {
+                validValues.addAll(possibleValues.stream()
+                        .map(Long::toBinaryString)
+                        .map(binaryString -> "0000000000".substring(binaryString.length()) + binaryString).toList());
+            } else {
+                List<String> newValidValues = new ArrayList<>();
+                for (String v : validValues) {
+                    for (Integer possibleValue : possibleValues) {
+                        String binaryString = Integer.toBinaryString(possibleValue);
+                        binaryString = "0000000000".substring(binaryString.length()) + binaryString;
+                        String byteStart = binaryString.substring(0, binaryString.length() - 3);
+                        String last3Bit = binaryString.substring(binaryString.length() - 3);
+                        if (v.endsWith(byteStart)) {
+                            newValidValues.add(v + last3Bit);
+                        }
+                    }
+                }
+                validValues = newValidValues;
+            }
+        }
+        return validValues.stream().map(v -> Long.parseLong(v, 2)).toList();
+    }
+
+    protected Map<Integer, List<Integer>> getPossibleValuesFiltered(Map<Integer, List<Integer>> possibleValues, Integer[] forcedValues) {
+        int byteSize = possibleValues.size() * 3;
+        Map<Integer, List<Integer>> possibleValuesFiltered = new HashMap<>();
+        for (int i = possibleValues.size() - 1; i >= 0; i--) {
+            // Last element should be less than 8
+            if (i == possibleValues.size() - 1) {
+                possibleValuesFiltered.put(i, possibleValues.get(i).stream().filter(v -> v < 8).toList());
+            } else if (i == possibleValues.size() - 2) {
+                possibleValuesFiltered.put(i, possibleValues.get(i).stream().filter(v -> v < Math.pow(8, 2) &&
+                        isValidWithForcedValues(List.of(forcedValues).subList(0, 6), v)).toList());
+            } else if (i == possibleValues.size() - 3) {
+                possibleValuesFiltered.put(i, possibleValues.get(i).stream().filter(v -> v < Math.pow(8, 3) &&
+                        isValidWithForcedValues(List.of(forcedValues).subList(0, 9), v)).toList());
+            } else {
+                int finalI = i;
+                possibleValuesFiltered.put(i, possibleValues.get(i).stream()
+                        .filter(v -> isValidWithForcedValues(List.of(forcedValues).subList(byteSize - finalI * 3 - 10, byteSize - finalI * 3), v)).toList());
+            }
+        }
+        return possibleValuesFiltered;
     }
 
     @Override
@@ -138,6 +289,6 @@ public class Day17 implements DailyExercise {
     @Override
     public String getSecondAnswer() {
         Input input = getInput("/day17/input.txt");
-        return Long.toString(getRegisterABruteForce(input.getRegister(), input.getProgram()));
+        return Long.toString(getMinRegisterA(input.getProgram()));
     }
 }
--- src/test/java/aoc/day17/Day17Test.java
+++ src/test/java/aoc/day17/Day17Test.java
@@ -1,13 +1,31 @@
 package aoc.day17;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.List;
 import java.util.stream.Stream;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertLinesMatch;
+import static org.junit.jupiter.api.Assertions.*;
 
 class Day17Test {
+    public static Stream<Arguments> isValidWithForcedValuesExamples() {
+        return Stream.of(
+                Arguments.of(List.of(-1), 1, true),
+                Arguments.of(List.of(1), 1, true),
+                Arguments.of(List.of(0), 1, false),
+                Arguments.of(List.of(0), 0, true),
+                Arguments.of(List.of(1, 0, 0), 4, true),
+                Arguments.of(List.of(1, 0, 1), 5, true),
+                Arguments.of(List.of(1, 1, 0), 6, true),
+                Arguments.of(List.of(1, 1, 1), 7, true),
+                Arguments.of(List.of(0, 1, 1, 1), 7, true),
+                Arguments.of(List.of(0, 1, 1, 1), 8, false)
+        );
+    }
+
     @Test
     void getInput_returnInput() {
         Input result = new Day17().getInput("/day17/example1.txt");
@@ -29,5 +47,48 @@ class Day17Test {
     void getRegisterABruteForce_ShouldWork_ifExample2() {
         Input input = new Day17().getInput("/day17/example2.txt");
         assertEquals("117440", Long.toString(new Day17().getRegisterABruteForce(input.getRegister(), input.getProgram())));
+    }
+
+    @Test
+    void getPossibleValues_shouldThrow_ifDoesntEndWell() {
+        List<Integer> program = List.of(0, 0, 1, 1, 2, 2, 3, 3, 4, 4);
+        assertThrows(RuntimeException.class, () -> new Day17().getPossibleValues(program, 0));
+    }
+
+    @Test
+    void getPossibleValues_shouldReturn() {
+        List<Integer> program = List.of(2, 4, 1, 1, 7, 5, 1, 5, 4, 3, 5, 5, 0, 3, 3, 0);
+        List<Integer> result = new Day17().getPossibleValues(program, 2);
+        List<Integer> expected = List.of(6, 14, 22, 30, 34, 38, 46, 53, 54, 61, 62, 68, 70, 71, 76, 78, 79, 84, 86, 87, 92, 94, 95, 98, 102, 103, 110, 111, 118, 119, 126, 127, 162, 181, 189, 226, 290, 309, 317, 324, 332, 340, 348, 354, 418, 437, 445, 482, 546, 565, 573, 580, 583, 588, 591, 596, 599, 604, 607, 610, 615, 623, 631, 639, 674, 693, 701, 738, 802, 821, 829, 836, 844, 852, 860, 866, 930, 949, 957, 994);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void getPossibleValues_shouldWork() {
+        Input input = new Day17().getInput("/day17/input.txt");
+        List<Integer> expected = List.of(4);
+        assertEquals(expected, new Day17().getPossibleValues(input.getProgram()).get(15));
+    }
+
+    @Test
+    void getForcedValues_shouldWork() {
+        Input input = new Day17().getInput("/day17/input.txt");
+        Integer[] expected = new Integer[]{
+                1, 0, 0, 1, 0, 1, -1, -1, -1, -1, -1, 0, 1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1
+        };
+        assertArrayEquals(expected, new Day17().getForcedValues(input.getProgram(), new Day17().getPossibleValues(input.getProgram())));
+    }
+
+    @ParameterizedTest
+    @MethodSource("isValidWithForcedValuesExamples")
+    void isValidWithForcedValues_shouldWork(List<Integer> forcedValues, int valueToTest, boolean expected) {
+        assertEquals(expected, new Day17().isValidWithForcedValues(forcedValues, valueToTest));
+    }
+
+    @Test
+    void getMinRegisterA_shouldWork() {
+        Input input = new Day17().getInput("/day17/input.txt");
+        Long expected = 164278899142333L;
+        assertEquals(expected, new Day17().getMinRegisterA(input.getProgram()));
     }
 }
